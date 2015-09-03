e861751c8e5e4ec8b687bae939399792b8b70fcb
digiwiz <danielterenzio@daniels-mbp.gianttiger.com>
2015-09-03 13:14:40 -0400

    store number
    store number

--- index.php
+++ index.php
@@ -199,7 +199,7 @@ function display($collection,&$up,$limit,$type) {
 
 			$name = preg_replace('/Tigre Géant /', '', preg_replace('/Giant Tiger /', '', $page['name'], 1), 1);
 //			$storeNumber = substr( ( preg_replace('/Tigre Géant /', '', preg_replace('/Giant Tiger /', '', $page['description'], 1), 1) ) , 0, 4);
-			$storeNumber = '#' . $page['store_number'];
+			$storeNumber = $page['store_number'];
 			$province = $page['location']['state'];
 			$link = $page['link'];
 			$id = $page['id'];
